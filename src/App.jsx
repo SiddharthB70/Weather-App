0d24da4caa580978ed89e3a69cf9bc716ed400ba
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-04-17 20:42:14 +0530

    added contorlled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import { useEffect, useState } from "react";
 
-const App = () => {};
+const App = () => {
+    const [country, setCountry] = useState("");
+
+    const onChange = (e) => {
+        setCountry(e.target.value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={country}
+                    onChange={onChange}
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
